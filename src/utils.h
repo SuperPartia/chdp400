c6ea489ca6c75881a6db1e15f9cdce7f3f8b4aec
Norbert Kubiak <norbert@nativetap.io>
2016-08-23 22:23:29 +0200

    Drobne zmiany

--- src/utils.h
+++ src/utils.h
@@ -9,14 +9,17 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#define endLine 1
+#define switchRec 2
+
 void initAll();
-uint8_t initialdiode (uint8_t mode)
+uint8_t initialdiode (uint8_t mode);
 
 void runConfig(uint16_t *samplingT, int *measurementTime, int *cooldownTime,
 		uint8_t *mode, uint8_t *repeats);
 
 bool switchReciever (uint8_t recieverCount);
-uint8_t doTheSwitching(uint8_t *diode, bool *reciever, mode);
+uint8_t doTheSwitching(uint8_t *diode, bool *reciever, uint8_t mode);
 
 
 
